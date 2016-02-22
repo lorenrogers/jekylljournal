80d907f5cf1e2f31839c81e264b45fc92dcad86f
Loren Rogers <loren@lorentrogers.com>
2016-02-21 23:03:31 -0500

    Adds save function

--- lib/jekylljournal/journalhandler.rb
+++ lib/jekylljournal/journalhandler.rb
@@ -1,6 +1,7 @@
 # This class is used to handle Jekyll Journals.
 # It is generally called from the command line wrapper.
 class Journalhandler
+
   # Loads the config dotfile from the default directory and sets the
   # application options
   def initialize
@@ -41,10 +42,25 @@ eol
     system 'vim', file_name
   end
 
-  # Adds and commits all new posts to git, then
+  # Adds and commits all new posts and assets to git, then
   # pushes to the default repositories.
   # TODO
   def save
     puts 'Saving...'
+    run_git_cmd 'add _posts/*'
+    run_git_cmd 'add assets/*'
+    # TODO: this should use more flexible values.
+    # Date format should be a dotfile option
+    # Message format should be a dotfile option
+    run_git_cmd "commit -m \"Save: #{Date.today.strftime '%Y-%m-%d'}\""
+    run_git_cmd 'push'
+    puts 'done.'
+  end
+
+  private
+
+  def run_git_cmd cmd
+    location = @options[:blog_location]
+    `git --git-dir=#{location}/.git --work-tree=#{location} #{cmd}`
   end
 end
